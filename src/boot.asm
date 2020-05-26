7f9bf3c8370e5316c5f8219c73eaac53d4c474c9
Stephen Grice <stephengrice@live.com>
2020-05-25 22:50:42 -0400

    Add code for OS7

--- src/boot.asm
+++ src/boot.asm
@@ -1,27 +1,22 @@
 bits 16
 [org 0x7c00]
-	mov si, STR1
-	call print_string
+
+	call enter_protected
 	
-	mov al, 0x2	; number of sectors: 2
-	mov bx, 0x0 	; read memory es:bx
-	mov es, bx	; 0x0:0x9000 = 0x9000
-	mov bx, 0x9000
-	call disk_read
-
-	; Print out the character at location read from disk
-	mov ah, 0xe
-	mov al, [0x9200]
-	int 0x10
 	jmp $
+	
+	%include "src/inc/gdt.asm"
+	%include "src/inc/enter_protected.asm"
 
-	STR1: db "Reading from 0x9200: ",0
-
-	%include "src/inc/print_string.asm"
-	%include "src/inc/disk_read.asm"
+[bits 32]
+pm_start:
+	; Print a character
+	mov ah, 0x0F
+	mov al, 'h'
+	mov [0xb8000], ax
+	mov al, 'i'
+	mov [0xb8002], ax
+	jmp $
 
 	times 510-($-$$) db 0
 	dw 0xaa55
-
-times 512 db 'f'
-times 512 db 'x'
