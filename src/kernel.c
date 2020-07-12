50724f2078331f2ad452a21f45540aa6b9423851
Stephen Grice <stephengrice@live.com>
2020-07-11 20:48:06 -0400

    Reorg kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -4,26 +4,23 @@
 extern void print_char_with_asm(char c, int row, int col);
 extern void load_gdt();
 
-void main()
-{
-	load_gdt();
-	char* mem = (char*) 0xb8000;
+void print_message() {
 	// Fill the screen with 'x'
 	int i, j;
 	for (i = 0; i < COLS; i++) {
 		for (j = 0; j < ROWS; j++) {
-			print_char_with_asm('Q',j,i);
+			print_char_with_asm('*',j,i);
 		}
 	}
-	// mem[0] = '-';
-	// mem[2] = 'O';
-	// mem[4] = 'S';
-	// mem[6] = '9';
-	// mem[8] = '-';
 	print_char_with_asm('-',0,0);
-	print_char_with_asm('O',0,1);
-	print_char_with_asm('S',0,2);
-	print_char_with_asm('1',0,3);
-	print_char_with_asm('1',0,4);
+	print_char_with_asm('P',0,1);
+	print_char_with_asm('K',0,2);
+	print_char_with_asm('O',0,3);
+	print_char_with_asm('S',0,4);
 	print_char_with_asm('-',0,5);
+}
+
+void main() {
+	load_gdt();
+	print_message();
 }
