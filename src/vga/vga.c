e952cc4ed43bbe7016969a5da3cdca5bde520d0a
Stephen Grice <stephengrice@live.com>
2022-04-12 05:40:35 -0400

    Copy in some OSdev code and get it to compile

--- src/vga/vga.c
+++ src/vga/vga.c
@@ -1,6 +1,101 @@
 #include "vga.h"
+#include "../kernel/kernel.h"
 #include "../screen/screen.h"
 
 void vga_test() {
     println("Not yet implemented!!", 21);
 }
+
+// Begin copied code
+// Source: https://files.osdev.org/mirrors/geezer/osd/graphics/modes.c
+// Changes:
+// - Initial: only grabbed code I thought was relevant to changing mode and displaying our first pixel
+// - Changed ioport_in, ioport_out funcs to instead point to ioport_in, ioport_out funcs defined in kernel.asm
+#define	VGA_AC_INDEX		0x3C0
+#define	VGA_AC_WRITE		0x3C0
+#define	VGA_AC_READ		    0x3C1
+#define	VGA_INSTAT_READ		0x3DA
+#define	VGA_MISC_WRITE		0x3C2
+#define	VGA_MISC_READ		0x3CC
+
+/*			COLOR emulation		MONO emulation */
+#define VGA_CRTC_INDEX		0x3D4		/* 0x3B4 */
+#define VGA_CRTC_DATA		0x3D5		/* 0x3B5 */
+#define VGA_GC_INDEX 		0x3CE
+#define VGA_GC_DATA 		0x3CF
+#define VGA_SEQ_INDEX		0x3C4
+#define VGA_SEQ_DATA		0x3C5
+
+#define	VGA_NUM_AC_REGS		21
+#define	VGA_NUM_CRTC_REGS	25
+#define	VGA_NUM_GC_REGS		9
+#define	VGA_NUM_SEQ_REGS	5
+
+unsigned char g_320x200x256[] =
+{
+/* MISC */
+	0x63,
+/* SEQ */
+	0x03, 0x01, 0x0F, 0x00, 0x0E,
+/* CRTC */
+	0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
+	0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x9C, 0x0E, 0x8F, 0x28,	0x40, 0x96, 0xB9, 0xA3,
+	0xFF,
+/* GC */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x05, 0x0F,
+	0xFF,
+/* AC */
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+	0x41, 0x00, 0x0F, 0x00,	0x00
+};
+void write_regs(unsigned char *regs)
+{
+	unsigned i;
+
+/* write MISCELLANEOUS reg */
+	ioport_out(VGA_MISC_WRITE, *regs);
+	regs++;
+/* write SEQUENCER regs */
+	for(i = 0; i < VGA_NUM_SEQ_REGS; i++)
+	{
+		ioport_out(VGA_SEQ_INDEX, i);
+		ioport_out(VGA_SEQ_DATA, *regs);
+		regs++;
+	}
+/* unlock CRTC registers */
+	ioport_out(VGA_CRTC_INDEX, 0x03);
+	ioport_out(VGA_CRTC_DATA, ioport_in(VGA_CRTC_DATA) | 0x80);
+	ioport_out(VGA_CRTC_INDEX, 0x11);
+	ioport_out(VGA_CRTC_DATA, ioport_in(VGA_CRTC_DATA) & ~0x80);
+/* make sure they remain unlocked */
+	regs[0x03] |= 0x80;
+	regs[0x11] &= ~0x80;
+/* write CRTC regs */
+	for(i = 0; i < VGA_NUM_CRTC_REGS; i++)
+	{
+		ioport_out(VGA_CRTC_INDEX, i);
+		ioport_out(VGA_CRTC_DATA, *regs);
+		regs++;
+	}
+/* write GRAPHICS CONTROLLER regs */
+	for(i = 0; i < VGA_NUM_GC_REGS; i++)
+	{
+		ioport_out(VGA_GC_INDEX, i);
+		ioport_out(VGA_GC_DATA, *regs);
+		regs++;
+	}
+/* write ATTRIBUTE CONTROLLER regs */
+	for(i = 0; i < VGA_NUM_AC_REGS; i++)
+	{
+		(void)ioport_in(VGA_INSTAT_READ);
+		ioport_out(VGA_AC_INDEX, i);
+		ioport_out(VGA_AC_WRITE, *regs);
+		regs++;
+	}
+/* lock 16-color palette and unblank display */
+	(void)ioport_in(VGA_INSTAT_READ);
+	ioport_out(VGA_AC_INDEX, 0x20);
+}
+// End copied code
