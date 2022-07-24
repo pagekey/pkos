103f20573160d70b13e9a680fd2f264d6426f158
Stephen Grice <stephengrice@live.com>
2022-07-24 09:33:37 -0400

    Add working test for stdlib streq

--- test/unit/stdlib.cpp
+++ test/unit/stdlib.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
+#include "../../src/common/stdlib.h"
 
-
-TEST( ExampleTest, SubExampleTest ) {
-    ASSERT_TRUE(1 == 1);
+TEST( stdlib, streq_equal ) {
+    char* mystring = "hello";
+    char* myotherstring = "hello";
+    ASSERT_TRUE(streq(mystring, 5, myotherstring, 5));
+}
+TEST( stdlib, streq_not_equal ) {
+    char* mystring = "hello";
+    char* myotherstring = "hello2";
+    ASSERT_FALSE(streq(mystring, 5, myotherstring, 6));
 }
