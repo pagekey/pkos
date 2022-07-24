f29467a22d2a1f76920efb130e80d1d17d65aec1
Stephen Grice <stephengrice@live.com>
2022-07-24 09:17:57 -0400

    Modify directory structure
    Use the `build` directory to make all these files

---
+++ test/unit/stdlib.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+
+TEST( ExampleTest, SubExampleTest ) {
+    ASSERT_TRUE(1 == 1);
+}
--- test/unit/suite.cpp
+++ test/unit/suite.cpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-TEST( ExampleTest, SubExampleTest ) {
-    ASSERT_TRUE(1 == 1);
-}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
