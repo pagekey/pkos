5d982f8c2a45208f199d4adc3305f02806f59401
Stephen Grice <stephengrice@live.com>
2023-02-16 21:25:18 -0500

    Add inl, outl, u8, u16, u32
    Also clean up lspci

--- src/common/types.h
+++ src/common/types.h
@@ -5,4 +5,8 @@
 #define false 0
 #define true 1
 
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
 #endif
--- src/pci/pci.c
+++ src/pci/pci.c
@@ -1,29 +1,31 @@
+#include "../common/types.h"
+
 #define CONFIG_ADDRESS_PORT 0xCF8
 #define CONFIG_DATA_PORT 0xCFC
 
 void lspci() {
     println("hello pci");
     // Setup address variable
-    unsigned int address = 0;
+    u32 address = 0;
     address |= 0x80000000; // bit 31: enable = 1
     // bits 30-24: reserved, leave them as 0
-    unsigned char bus = 0; // 8 bits
-    unsigned char device = 0; // 8 bits
-    unsigned char function = 0; // 3 bits
-    unsigned char offset = 0; // 8 bits
-    address |= (bus << 23); // bits 23-16: bus
-    address |= (device << 15); // bits 15-11: device
+    u8 bus = 0; // 8 bits
+    u8 device = 0; // 8 bits
+    u8 function = 0; // 3 bits
+    u8 offset = 0; // 8 bits
+    address |= (bus << 16); // bits 23-16: bus
+    address |= (device << 11); // bits 15-11: device
     function &= 0b111;
-    address |= (function << 10); // bits 10-8: function
+    address |= (function << 8); // bits 10-8: function
     address |= offset; // bits 7-0: offset
 
     // Set the PCI address to read
-    ioport_out(CONFIG_ADDRESS_PORT, address);
+    outl(CONFIG_ADDRESS_PORT, address);
 
     // Read the value!
-    unsigned int retval = ioport_in(CONFIG_DATA_PORT); // this only reads 16 bits??
+    u32 retval = inl(CONFIG_DATA_PORT);
     println("read:");
-    char result[10];
+    u8 result[10];
     print("0x");
     println(itoah(retval, *result));
 }
