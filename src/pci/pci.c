0f006e8f245e54da051bd58b8e9647c983c487db
Stephen Grice <stephengrice@live.com>
2023-02-16 21:31:54 -0500

    Create read_pci_port function

--- src/pci/pci.c
+++ src/pci/pci.c
@@ -3,27 +3,30 @@
 #define CONFIG_ADDRESS_PORT 0xCF8
 #define CONFIG_DATA_PORT 0xCFC
 
-void lspci() {
-    println("hello pci");
-    // Setup address variable
+u32 read_pci_port(u8 bus, u8 device, u8 function, u8 offset) {
     u32 address = 0;
+    
+    // Clear out any bits not within range
+    function &= 0b111; // 3 bits
+    
+    // Set the bits
     address |= 0x80000000; // bit 31: enable = 1
     // bits 30-24: reserved, leave them as 0
-    u8 bus = 0; // 8 bits
-    u8 device = 0; // 8 bits
-    u8 function = 0; // 3 bits
-    u8 offset = 0; // 8 bits
     address |= (bus << 16); // bits 23-16: bus
     address |= (device << 11); // bits 15-11: device
-    function &= 0b111;
     address |= (function << 8); // bits 10-8: function
     address |= offset; // bits 7-0: offset
 
-    // Set the PCI address to read
+    // Set the address to read
     outl(CONFIG_ADDRESS_PORT, address);
 
-    // Read the value!
-    u32 retval = inl(CONFIG_DATA_PORT);
+    // Read data from PCI
+    return inl(CONFIG_DATA_PORT);
+}
+
+void lspci() {
+    println("hello pci");
+    u32 retval = read_pci_port(0, 0, 0, 5);
     println("read:");
     u8 result[10];
     print("0x");
