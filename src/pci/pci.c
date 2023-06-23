0e715020cf7c401664960c91943ecba5f7ec40b6
Stephen Grice <stephengrice@live.com>
2023-06-23 12:44:41 -0400

    Get lspci working in one line

--- src/pci/pci.c
+++ src/pci/pci.c
@@ -26,30 +26,35 @@ u32 read_pci_port(u8 bus, u8 device, u8 function, u8 offset) {
 
 void lspci() {
     u8 string_rep[10];
-    for (u8 i = 0; i < 2; i++) {
-        for (u8 j = 0; j < 2; j++) {
-            for (u8 k = 0; k < 2; k++) {
+    for (u8 i = 0; i < 255; i++) {
+        for (u8 j = 0; j < 255; j++) {
+            for (u8 k = 0; k < 8; k++) {
+                u32 pci_data = read_pci_port(i, j, k, 0);
+                u16 vendor_id = pci_data & 0xffff;
+                u16 device_id = (pci_data >> 16) & 0xffff;
+                pci_data = read_pci_port(i, j, k, 0x09);
+                u8 base_class = (pci_data >> 16) & 0xff;
+                u8 sub_class = (pci_data >> 8) & 0xff;
+                u8 progInterface = pci_data & 0xff;
+                if (vendor_id == 0xFFFF) {
+                    continue;
+                }
                 print("Bus ");
                 print(itoa(i, string_rep));
                 print(" Device ");
                 print(itoa(j, string_rep));
                 print(" Function ");
-                print(itoa(j, string_rep));
-                print(": ");
-                u32 pci_data = read_pci_port(i, j, k, 0);
-                u16 vendor_id = pci_data & 0xffff;
-                print(" Vendor=");
+                print(itoa(k, string_rep));
+                print(": Vendor=");
                 print(itoah(vendor_id, *string_rep));
-                u16 device_id = (pci_data >> 16) & 0xffff;
                 print(" Device=");
                 print(itoah(device_id, *string_rep));
-                pci_data = read_pci_port(i, j, k, 0x0a);
-                u8 base_class = (pci_data >> 8) & 0xff;
-                u8 sub_class = pci_data & 0xff;
-                print(" Base Class=");
+                print(" Class=");
                 print(itoah(base_class, *string_rep));
                 print(" SubClass=");
-                println(itoah(sub_class, *string_rep));
+                print(itoah(sub_class, *string_rep));
+                print(" ProgIf=");
+                println(itoah(progInterface, *string_rep));
             }
         }
     }
